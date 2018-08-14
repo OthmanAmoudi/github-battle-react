6e3317294b6afcfb28d836c6fc59ef0c3ddfab61
Othman <red.fantastic@hotmail.com>
2018-08-14 06:13:53 +0300

    :boom:

--- src/App.js
+++ src/App.js
@@ -3,6 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleLanguage = this.handleLanguage.bind(this);
+
+    this.state = {
+      languages: ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'],
+      selectedLanguage: 'All'
+    };
+  }
+
+  handleLanguage(lang){
+    this.setState(()=>{
+      return {
+        selectedLanguage: lang
+      }
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +29,18 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/* ***********   end of HEADER   ******** */}
+        <ul className="App-intro">
+          {this.state.languages.map(lang => {
+            return (
+              <li
+              className={this.state.selectedLanguage === lang ? "selected" : ""}
+              key={lang} onClick={this.handleLanguage.bind(null,lang)}>
+                {lang}
+              </li>
+            );
+          })}
+        </ul>
       </div>
     );
   }
